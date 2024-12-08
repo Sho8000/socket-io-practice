ff79925202c7d7c0d7742e728540a7026f204b00
Sho8000 <yoshikichi8000@gmail.com>
2024-12-08 13:47:10 -0800

    messages being updaged in ui

--- src/index.ts
+++ src/index.ts
@@ -10,18 +10,27 @@ io.on("connection", (socket)=>{
   console.log('A user connected', socket.id);
 
   socket.on('joinRoom',(room,num1) => {
-    console.log('on JoinRoom event & extra data = ',num1);
-
-    socket.join(room);
+      socket.join(room);
     console.log(`User ${socket.id} joined room ${room}`);
     socket.join(room);
+  })
+
+  socket.on('leaveRoom', room => {
+    console.log(`User ${socket.id} left room ${room}`)
+    socket.leave(room);
+  })
+
+  socket.on('message', ({room, message}) => {
+    console.log("recieved on server:", message);
+
+    io.to(room).emit('message',{id:socket.id, message})
   })
 
   socket.on('disconnect', ()=>{
     console.log('A user disconnected', socket.id)
   })
 })
- 
+
 server.listen(PORT, () => {
   console.log(`[server]: Listening at http://${HOST}:${PORT}`);
 });
--- src/public/main.js
+++ src/public/main.js
@@ -7,7 +7,10 @@ let currentRoom = null;
 const roomSelect = document.getElementById('roomSelect')
 const joinRoomButton = document.getElementById('joinRoomButton');
 const leaveRoomButton = document.getElementById('leaveRoomButton');
-const currentRoomSelected = document.getElementById('messageInput')
+const currentRoomSelected = document.getElementById('currentRoom');
+const messageInput = document.getElementById('messageInput');
+const sendButton = document.getElementById('sendButton');
+const messagesDiv = document.getElementById('messages');
 
 joinRoomButton.addEventListener('click',()=>{
   const room = roomSelect.value;
@@ -15,11 +18,29 @@ joinRoomButton.addEventListener('click',()=>{
     socket.emit('leaveRoom', currentRoom);
   }
   currentRoom = room;
+  currentRoomSelected.innerText = currentRoom;
   socket.emit('joinRoom',room,99)
 })
-
-leaveRoomButton.addEventListener('click', {
+leaveRoomButton.addEventListener('click', ()=>{
   if(currentRoom){
     socket.emit('leaveRoom', currentRoom);
+    currentRoom =null;
+    currentRoomSelected.innerText = "None";
   }
+})
+sendButton.addEventListener('click', ()=> {
+  const message = messageInput.value;
+  if(currentRoom){
+    socket.emit('message',{room:currentRoom, message});
+    messageInput.value = '';
+  }
+})
+
+socket.on('message',({id, message}) => {
+  console.log('received on client:', message)
+
+  const messageElement = document.createElement('div');
+  messageElement.textContent = `${id}:${message}`;
+
+  messagesDiv.appendChild(messageElement);
 })

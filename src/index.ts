68498118dc0c23346245a025a30219ebda84bc97
Sho8000 <yoshikichi8000@gmail.com>
2024-12-08 12:41:37 -0800

    start fix

--- src/index.ts
+++ src/index.ts
@@ -9,11 +9,19 @@ const io = new Server(server)
 io.on("connection", (socket)=>{
   console.log('A user connected', socket.id);
 
+  socket.on('joinRoom',(room,num1) => {
+    console.log('on JoinRoom event & extra data = ',num1);
+
+    socket.join(room);
+    console.log(`User ${socket.id} joined room ${room}`);
+    socket.join(room);
+  })
+
   socket.on('disconnect', ()=>{
     console.log('A user disconnected', socket.id)
   })
 })
-
+ 
 server.listen(PORT, () => {
   console.log(`[server]: Listening at http://${HOST}:${PORT}`);
 });
--- src/public/main.js
+++ src/public/main.js
@@ -1,3 +1,25 @@
 console.log("ready")
 
 const socket = io();
+
+let currentRoom = null;
+
+const roomSelect = document.getElementById('roomSelect')
+const joinRoomButton = document.getElementById('joinRoomButton');
+const leaveRoomButton = document.getElementById('leaveRoomButton');
+const currentRoomSelected = document.getElementById('messageInput')
+
+joinRoomButton.addEventListener('click',()=>{
+  const room = roomSelect.value;
+  if(currentRoom){
+    socket.emit('leaveRoom', currentRoom);
+  }
+  currentRoom = room;
+  socket.emit('joinRoom',room,99)
+})
+
+leaveRoomButton.addEventListener('click', {
+  if(currentRoom){
+    socket.emit('leaveRoom', currentRoom);
+  }
+})
